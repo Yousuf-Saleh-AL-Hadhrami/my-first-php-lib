435c4867e05837e4750d827cc369e9742142ef62
Yousuf AL-Hadhrami <izkiboy@gmail.com>
2025-02-24 13:44:27 +0400

    updated'

--- src/Math.php
+++ src/Math.php
@@ -7,13 +7,18 @@ class Math
 
     public $sum;
 
+    public function __construct()
+{
+    $this->sum = 0;
+}
+
+
     public function add($numbers = []){
 
-        for($i = 0 ; $i <= count($numbers); $i++){
-
+        for ($i = 0; $i < count($numbers); $i++) {
             $this->sum += $numbers[$i];
         }
-
+        
         return $this;
     }
 
